362017a22632ffa60ba2a1423caedf248c4352a4
Kavin <kavinmadusanka20011@gmail.com>
2026-03-23 17:38:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,8 @@ const app = express();
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(colors.green(`Server is running development mode on port ${PORT}`));
+});
